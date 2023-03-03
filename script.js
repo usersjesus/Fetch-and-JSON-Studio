6e4382e4e613552a22716d4f8dcfbbc1b21974c3
sjesus <s.jesus10@yahoo.com>
2023-03-02 19:56:14 -0600

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-// TODO: add code here
+window.addEventListener("load", (event) => {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+        let astronauts = document.getElementById("container")
+       for (let i = 0; i<data.length; i++) {
+        // let obj = data[i];
+        // console.log(obj);
+        astronauts.innerHTML += 
+        `<div class="astronaut">
+        <div class="bio">
+           <h3>${data[i].firstName} ${data[i].lastName}</h3>
+           <ul>
+              <li>Hours in space: ${data[i].hoursInSpace}</li>
+              <li>Active: ${data[i].active}</li>
+              <li>Skills: ${data[i].skills}</li>
+           </ul>
+        </div>
+        <img class="avatar" src="${data[i].picture}">
+     </div>`
+       };
+    })
+    
+
+    // function accessObjects() {
+    //     for (let item in data) {
+    //     let obj = data[item];
+    // }
+    // return obj;
+    // }
+  });
